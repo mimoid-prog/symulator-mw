7fcdc1e21bbc0140269e49e42f50dec1e8e6ed5f
sesa736123 <mimoid.prog@gmail.com>
2025-10-25 15:41:10 +0200

    Update global styles for Chakra UI integration and set HTML class for theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
  children: React.ReactNode;
 }>) {
  return (
-  <html lang="pl" suppressHydrationWarning>
+  <html lang="pl" suppressHydrationWarning className="chakra-theme">
    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
     <Provider>{children}</Provider>
    </body>
