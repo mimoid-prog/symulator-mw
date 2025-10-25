8b6cc423b551b22842ad4f5f904a259421fbc8f4
sesa736123 <mimoid.prog@gmail.com>
2025-10-25 14:41:12 +0200

    Refactor Header component to include ColorModeButton and improve layout structure

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,44 +1,48 @@
 import { Flex, Heading, Button, Portal, Popover, Text } from '@chakra-ui/react';
+import { ColorModeButton } from '@/components/ui/color-mode';
 import { LuMail } from 'react-icons/lu';
 
 const Header = () => {
  return (
   <Flex justifyContent="space-between" alignItems="center">
    <Heading color="teal.100">Symulator MW</Heading>
-   <Popover.Root>
-    <Popover.Trigger asChild>
-     <Button>
-      <LuMail />
-      Kontakt
-     </Button>
-    </Popover.Trigger>
-    <Portal>
-     <Popover.Positioner>
-      <Popover.Content>
-       <Popover.CloseTrigger />
-       <Popover.Header>Kontakt</Popover.Header>
-       <Popover.Body>
-        <Button
-         variant="link"
-         as="a"
-         href="https://www.margonem.pl/profile/view,8686722#char_359294,aldous"
-         target="_blank"
-        >
-         Link do profilu
-        </Button>
-        <Text fontSize="sm">Nick w grze: Vlk Romanov, świat Aldous</Text>
-        <Text fontSize="sm">Discord: shcopec#1329</Text>
-        <Text fontSize="sm">Email: rzyrkunka@gmail.com</Text>
-        <br />
-        <Text fontSize="sm">
-         Jeśli zauważyłeś błąd, masz jakąś propozycję lub pytanie to śmiało
-         odzywaj się do mnie.
-        </Text>
-       </Popover.Body>
-      </Popover.Content>
-     </Popover.Positioner>
-    </Portal>
-   </Popover.Root>
+   <Flex alignItems="center" gap="2">
+    <ColorModeButton />
+    <Popover.Root>
+     <Popover.Trigger asChild>
+      <Button>
+       <LuMail />
+       Kontakt
+      </Button>
+     </Popover.Trigger>
+     <Portal>
+      <Popover.Positioner>
+       <Popover.Content>
+        <Popover.CloseTrigger />
+        <Popover.Header>Kontakt</Popover.Header>
+        <Popover.Body>
+         <Button
+          variant="link"
+          as="a"
+          href="https://www.margonem.pl/profile/view,8686722#char_359294,aldous"
+          target="_blank"
+         >
+          Link do profilu
+         </Button>
+         <Text fontSize="sm">Nick w grze: Vlk Romanov, świat Aldous</Text>
+         <Text fontSize="sm">Discord: shcopec#1329</Text>
+         <Text fontSize="sm">Email: rzyrkunka@gmail.com</Text>
+         <br />
+         <Text fontSize="sm">
+          Jeśli zauważyłeś błąd, masz jakąś propozycję lub pytanie to śmiało
+          odzywaj się do mnie.
+         </Text>
+        </Popover.Body>
+       </Popover.Content>
+      </Popover.Positioner>
+     </Portal>
+    </Popover.Root>
+   </Flex>
   </Flex>
  );
 };
