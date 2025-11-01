5696d70731163c701781f9279315ade39a2867d9
sesa736123 <mimoid.prog@gmail.com>
2025-11-01 16:54:56 +0100

    Improve abilities perf

--- components/abilities.tsx
+++ components/abilities.tsx
@@ -1,87 +1,22 @@
 'use client';
-import { Box, Stack, Text, Button, Tag, Flex } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
-import { MouseEvent } from 'react';
+
 import store from '@/lib/Store';
+import { AbilityItem } from './ability-item';
 
 export const Abilities = observer(() => {
- const handlePointsChange = (
-  e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
-  id: number
- ) => {
-  e.preventDefault();
-
-  if (e.button === 0) {
-   store.changeAbilityPoints(true, id, e.ctrlKey ? true : false);
-  } else if (e.button === 2) {
-   store.changeAbilityPoints(false, id, e.ctrlKey ? true : false);
-  }
- };
-
- const getBorderColor = (abilityId: number, points: number) => {
-  return abilityId === 0
-   ? 'green.400'
-   : points === 0
-   ? 'red.400'
-   : points === 10
-   ? 'green.400'
-   : 'yellow.600';
- };
-
  return (
   <Box>
    <Stack gap="12px">
     <Heading size="xl">Umiejętności do użycia</Heading>
+
     {store.abilitiesWithState.map((abilityWithState) => (
-     <Button
-      variant="outline"
-      py="6px"
-      height="100%"
+     <AbilityItem
       key={abilityWithState.id}
-      onClick={(e) => handlePointsChange(e, abilityWithState.id)}
-      onContextMenu={(e) => handlePointsChange(e, abilityWithState.id)}
-      borderColor={getBorderColor(abilityWithState.id, abilityWithState.points)}
-      disabled={abilityWithState.id === 0}
-     >
-      {abilityWithState.new && (
-       <Tag.Root
-        colorPalette="blue"
-        variant="outline"
-        size="sm"
-        style={{
-         position: 'absolute',
-         top: 8,
-         left: 8,
-        }}
-       >
-        <Tag.Label>Nowość</Tag.Label>
-       </Tag.Root>
-      )}
-
-      <Tag.Root
-       colorPalette="blue"
-       size="sm"
-       style={{
-        position: 'absolute',
-        top: 8,
-        right: 8,
-       }}
-      >
-       <Tag.Label>lvl: {abilityWithState.minLevel}</Tag.Label>
-      </Tag.Root>
-
-      <Flex direction="column" gap="1">
-       <Text>{abilityWithState.name}</Text>
-       {abilityWithState.id !== 0 && (
-        <Text fontSize="2xl">{abilityWithState.points}/10</Text>
-       )}
-       <Text>
-        Koszty | Mana: {abilityWithState.manaCost}, Energia{' '}
-        {abilityWithState.energyCost}
-       </Text>
-      </Flex>
-     </Button>
+      abilityWithState={abilityWithState}
+     />
     ))}
    </Stack>
   </Box>
---
+++ components/ability-item.tsx
@@ -0,0 +1,84 @@
+'use client';
+import { Text, Button, Tag, Flex } from '@chakra-ui/react';
+import { observer } from 'mobx-react-lite';
+import { MouseEvent } from 'react';
+import store from '@/lib/Store';
+import { AbilityWithState } from '@/types/AbilityWithState';
+
+export const AbilityItem = observer(
+ ({ abilityWithState }: { abilityWithState: AbilityWithState }) => {
+  const handlePointsChange = (
+   e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+   id: number
+  ) => {
+   e.preventDefault();
+
+   if (e.button === 0) {
+    store.changeAbilityPoints(true, id, e.ctrlKey ? true : false);
+   } else if (e.button === 2) {
+    store.changeAbilityPoints(false, id, e.ctrlKey ? true : false);
+   }
+  };
+
+  const getBorderColor = (abilityId: number, points: number) => {
+   return abilityId === 0
+    ? 'green.400'
+    : points === 0
+    ? 'red.400'
+    : points === 10
+    ? 'green.400'
+    : 'yellow.600';
+  };
+
+  return (
+   <Button
+    variant="outline"
+    py="6px"
+    height="100%"
+    key={abilityWithState.id}
+    onClick={(e) => handlePointsChange(e, abilityWithState.id)}
+    onContextMenu={(e) => handlePointsChange(e, abilityWithState.id)}
+    borderColor={getBorderColor(abilityWithState.id, abilityWithState.points)}
+    disabled={abilityWithState.id === 0}
+   >
+    {abilityWithState.new && (
+     <Tag.Root
+      colorPalette="blue"
+      variant="outline"
+      size="sm"
+      style={{
+       position: 'absolute',
+       top: 8,
+       left: 8,
+      }}
+     >
+      <Tag.Label>Nowość</Tag.Label>
+     </Tag.Root>
+    )}
+
+    <Tag.Root
+     colorPalette="blue"
+     size="sm"
+     style={{
+      position: 'absolute',
+      top: 8,
+      right: 8,
+     }}
+    >
+     <Tag.Label>lvl: {abilityWithState.minLevel}</Tag.Label>
+    </Tag.Root>
+
+    <Flex direction="column" gap="1">
+     <Text>{abilityWithState.name}</Text>
+     {abilityWithState.id !== 0 && (
+      <Text fontSize="2xl">{abilityWithState.points}/10</Text>
+     )}
+     <Text>
+      Koszty | Mana: {abilityWithState.manaCost}, Energia{' '}
+      {abilityWithState.energyCost}
+     </Text>
+    </Flex>
+   </Button>
+  );
+ }
+);
